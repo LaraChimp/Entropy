301235a742b8f91cd9ed4b3679f4cf94569cfc75
Percy Mamedy <percymamedy@gmail.com>
2020-06-05 16:24:06 +0400

    Remove .php_cs.cache

--- src/EntropyServiceProvider.php
+++ src/EntropyServiceProvider.php
@@ -6,5 +6,21 @@ use Illuminate\Support\ServiceProvider;
 
 class EntropyServiceProvider extends ServiceProvider
 {
-    //
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
 }
